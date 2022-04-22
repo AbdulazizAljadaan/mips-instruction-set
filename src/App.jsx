e5741756d07494d2bbda7db4eb0ffc63a204bf2d
Abdulaziz Aljadaan <abdulazizaljadaan1990@gmail.com>
2022-04-22 18:37:19 +0300

    added Container component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Container } from '@mui/material'
 import React from 'react'
 import Format from './components/Format'
 import SelectInstruction from './components/SelectInstruction'
@@ -7,11 +8,13 @@ function App() {
   const [instructionData, setInstructionData] = useInstruction()
 
   return (
-    <div className="space-y-5">
-      <h1 className="text-2xl text-center">Core Instruction Set</h1>
-      <SelectInstruction setInstruction={name => setInstructionData(name)} />
-      {instructionData && <Format {...instructionData} />}
-    </div>
+    <Container>
+      <div className="space-y-5">
+        <h1 className="text-2xl text-center">Core Instruction Set</h1>
+        <SelectInstruction setInstruction={name => setInstructionData(name)} />
+        {instructionData && <Format {...instructionData} />}
+      </div>
+    </Container>
   )
 }
 
